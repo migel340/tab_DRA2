ba7edd55da883d548a55404e604efd6b6a5c1ba2
migel340 <loke34w@gmail.com>
2026-03-22 13:36:07 +0100

    initial commit

---
+++ dra2/src/main/java/com/tab/dra2/Dra2Application.java
@@ -0,0 +1,13 @@
+package com.tab.dra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dra2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dra2Application.class, args);
+	}
+
+}
---
+++ dra2/src/test/java/com/tab/dra2/Dra2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tab.dra2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dra2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
