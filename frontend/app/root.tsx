fac1b85d0204fc86bade2bdb10bb9b4a6e6a19ce
Dawid Kozaczuk <dk312091@student.polsl.pl>
2026-05-06 13:54:43 +0200

    chore(frontend): remove QueryClient and related dependencies

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -7,10 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import { QueryClientProvider } from "@tanstack/react-query";
 import type { Route } from "./+types/root";
 import "./app.css";
-import { queryClient } from "./config/queryClient";
 import { Toaster } from "./components/ui/sonner";
 
 export const links: Route.LinksFunction = () => [
@@ -46,11 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />;
-    </QueryClientProvider>
-  );
+  return <Outlet />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
