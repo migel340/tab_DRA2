f6a64a1a30f92ae49b1ae36affa0ee4803c34f53
Dawid Kozaczuk <dk312091@student.polsl.pl>
2026-05-14 13:43:46 +0200

    fix: api-url

--- frontend/app/routes/auth/auth-api.ts
+++ frontend/app/routes/auth/auth-api.ts
@@ -3,7 +3,7 @@ import type { LoginData, AuthResponse } from "~/types/auth";
 
 export const authApi = {
   login: async (payload: LoginData): Promise<AuthResponse> => {
-    return api<AuthResponse>("/api/auth/login", {
+    return api<AuthResponse>("/auth/login", {
       method: "POST",
       body: JSON.stringify(payload),
       withAuth: false,
