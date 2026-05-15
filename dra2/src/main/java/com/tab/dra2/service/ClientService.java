8a00d641eafd14a8ada1f17a3135685c97f98241
migel340 <loke34w@gmail.com>
2026-05-15 19:24:15 +0200

    chore(client-service): remove unused Locale import from ClientService

--- dra2/src/main/java/com/tab/dra2/service/ClientService.java
+++ dra2/src/main/java/com/tab/dra2/service/ClientService.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.NoSuchElementException;
 
 @Service
